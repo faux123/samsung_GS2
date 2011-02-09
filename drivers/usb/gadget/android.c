87ff29f5ac0ca5de25a59f838a7f58526b378024
Mike Lockwood <lockwood@android.com>
2011-02-09 09:38:26 -0500

    USB: gadget: android: Support switching vendor ID when configuration changes
    Based on the list of enabled USB functions, we can now switch the vendor ID
    as well as the product ID.
    
    Signed-off-by: Mike Lockwood <lockwood@android.com>
    [resolved conflicts in drivers/usb/gadget/android.c]
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    
    Change-Id: I900b95dfa12588c9f54075696ab3c9691602ee8d
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -77,6 +77,7 @@ struct android_dev {
 	int num_functions;
 	char **functions;
 
+	int vendor_id;
 	int product_id;
 	int version;
 #ifdef CONFIG_USB_ANDROID_SAMSUNG_COMPOSITE
@@ -296,6 +297,22 @@ static int product_matches_functions(struct android_usb_product *p)
 	return 1;
 }
 
+static int get_vendor_id(struct android_dev *dev)
+{
+	struct android_usb_product *p = dev->products;
+	int count = dev->num_products;
+	int i;
+
+	if (p) {
+		for (i = 0; i < count; i++, p++) {
+			if (p->vendor_id && product_matches_functions(p))
+				return p->vendor_id;
+		}
+	}
+	/* use default vendor ID */
+	return dev->vendor_id;
+}
+
 static int get_product_id(struct android_dev *dev)
 {
 	struct android_usb_product *p = dev->products;
@@ -316,7 +333,7 @@ static int __devinit android_bind(struct usb_composite_dev *cdev)
 {
 	struct android_dev *dev = _android_dev;
 	struct usb_gadget	*gadget = cdev->gadget;
-	int			gcnum, id, product_id, ret;
+	int			gcnum, id, ret;
 
 	pr_debug("android_bind\n");
 
@@ -382,8 +399,8 @@ static int __devinit android_bind(struct usb_composite_dev *cdev)
 
 	usb_gadget_set_selfpowered(gadget);
 	dev->cdev = cdev;
-	product_id = get_product_id(dev);
-	device_desc.idProduct = __constant_cpu_to_le16(product_id);
+	device_desc.idVendor = __constant_cpu_to_le16(get_vendor_id(dev));
+	device_desc.idProduct = __constant_cpu_to_le16(get_product_id(dev));
 	cdev->desc.idProduct = device_desc.idProduct;
 
 	return 0;
@@ -745,7 +762,6 @@ int android_enable_function(struct usb_function *f, int enable)
 {
 	struct android_dev *dev = _android_dev;
 	int disable = !enable;
-	int product_id;
 
 	if (!is_sysfschange_allowed(f))
 		return -EINVAL;
@@ -781,8 +797,9 @@ int android_enable_function(struct usb_function *f, int enable)
 			android_config_functions(f, enable);
 #endif
 
-		product_id = get_product_id(dev);
-		device_desc.idProduct = __constant_cpu_to_le16(product_id);
+		device_desc.idVendor = __constant_cpu_to_le16(get_vendor_id(dev));
+		device_desc.idProduct = __constant_cpu_to_le16(get_product_id(dev));
+
 		if (dev->cdev)
 			dev->cdev->desc.idProduct = device_desc.idProduct;
 		usb_composite_force_reset(dev->cdev);
@@ -1033,9 +1050,11 @@ static int __init android_probe(struct platform_device *pdev)
 		dev->num_products = pdata->num_products;
 		dev->functions = pdata->functions;
 		dev->num_functions = pdata->num_functions;
-		if (pdata->vendor_id)
+		if (pdata->vendor_id) {
+			dev->vendor_id = pdata->vendor_id;
 			device_desc.idVendor =
 				__constant_cpu_to_le16(pdata->vendor_id);
+		}
 		if (pdata->product_id) {
 			dev->product_id = pdata->product_id;
 			device_desc.idProduct =
