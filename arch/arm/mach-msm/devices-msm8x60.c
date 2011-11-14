7bb22fc2ddef6790f00095612360015ca7a4ce9a
Suman Tatiraju <sumant@codeaurora.org>
2011-11-14 11:31:15 +0530

    msm: kgsl: New low power level
    Saves ~8ma at battery for low fps apps.
    
    Change-Id: I052d44b6d6a0bf070197257ef80da312a78d3d17
    CRs-Fixed: 318682
    Signed-off-by: Suman Tatiraju <sumant@codeaurora.org>

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -611,6 +611,15 @@ static struct msm_bus_vectors grp3d_init_vectors[] = {
 	},
 };
 
+static struct msm_bus_vectors grp3d_low_vectors[] = {
+	{
+		.src = MSM_BUS_MASTER_GRAPHICS_3D,
+		.dst = MSM_BUS_SLAVE_EBI_CH0,
+		.ab = 0,
+		.ib = 990000000U,
+	},
+};
+
 static struct msm_bus_vectors grp3d_nominal_low_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
@@ -642,6 +651,10 @@ static struct msm_bus_paths grp3d_bus_scale_usecases[] = {
 	{
 		ARRAY_SIZE(grp3d_init_vectors),
 		grp3d_init_vectors,
+	},
+	{
+		ARRAY_SIZE(grp3d_low_vectors),
+		grp3d_low_vectors,
 	},
 	{
 		ARRAY_SIZE(grp3d_nominal_low_vectors),
@@ -678,7 +691,7 @@ static struct msm_bus_vectors grp2d0_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = 1300000000U,
+		.ib = 990000000U,
 	},
 };
 
@@ -713,7 +726,7 @@ static struct msm_bus_vectors grp2d1_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE1,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = 1300000000U,
+		.ib = 990000000U,
 	},
 };
 
@@ -771,18 +784,22 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		.pwrlevel = {
 			{
 				.gpu_freq = 266667000,
-				.bus_freq = 3,
+				.bus_freq = 4,
 				.io_fraction = 0,
 			},
 			{
 				.gpu_freq = 228571000,
-				.bus_freq = 2,
+				.bus_freq = 3,
 				.io_fraction = 33,
 			},
 			{
 				.gpu_freq = 200000000,
-				.bus_freq = 1,
+				.bus_freq = 2,
 				.io_fraction = 100,
+			},
+			{
+				.gpu_freq = 177778000,
+				.bus_freq = 1
 			},
 			{
 				.gpu_freq = 27000000,
@@ -790,7 +807,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			},
 		},
 		.init_level = 0,
-		.num_levels = 4,
+		.num_levels = 5,
 		.set_grp_async = NULL,
 		.idle_timeout = HZ/5,
 #ifdef CONFIG_MSM_BUS_SCALING
